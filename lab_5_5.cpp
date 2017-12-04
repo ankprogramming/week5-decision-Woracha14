9cbdf6f5153a660811e0740596f358c83b6b13c4
Unknown <milk756@hotmail.com>
2017-12-04 18:20:10 +0700

    ทำข้อที่ 5 (แต่ยังไม่เสร็จ)

--- lab_5_5.cpp
+++ lab_5_5.cpp
@@ -1,45 +1,53 @@
 #include <iostream>
 #include <string>
 
-int main () {
+using namespace std;
 
-  string playerName;
-  int score = 0, life = 3;
+int main ()
+{
 
-  // ประกาศตัวแปรเพื่อรับการเลือกคำตอบของผู้เล่น
-  // เช่นการเป็น ตัวเลขให้ประกาศเป็น int choice1;
-  // ถ้ารับคำตอบเป็นตัวอักษร ให้ประกาศเป็น char choice1;
-  //
+    string playerName;
+    int score = 0, life = 3;
+    // ประกาศตัวแปรเพื่อรับการเลือกคำตอบของผู้เล่น
+    // เช่นการเป็น ตัวเลขให้ประกาศเป็น int choice1;
+    // ถ้ารับคำตอบเป็นตัวอักษร ให้ประกาศเป็น char choice1;
+    //
 
-  cout << "Welcome to Mini Adventure Game." << endl;
-  cout << "Please enter your name : ";
+    cout << "Welcome to Mini Adventure Game." << endl;
+    cout << "Please enter your name : ";
 
-  // เขียนคำสั่งรับชื่อ แล้วเก็บในตัวแปร playerName
+    // เขียนคำสั่งรับชื่อ แล้วเก็บในตัวแปร playerName
+    cin >> playerName;
 
+    if (playerName.length() > 8)
+    {
+        playerName.erase (playerName.begin()+8, playerName.end());
+        cout << "Please enter your name : " << playerName << endl;
+    }
 
-  // เขียนคำสั่งแสดงผล คำบรรยายสภาพแวดล้อม สถานการณ์ และคำทักทายที่มีชื่อตัวละคร
-  // เช่น cout << "Hello " << playerName << ". You are in Robinson. You are walking happily. Suddenly you realize that forgetting the wallet."
-  // << " But you're lazy to get back to get it." << endl;
-
-
-  // เขียนคำสั่งแสดงผล คำถามให้ผู้ใช้ตัดสินใจ
-  // เช่น cout << "What should you do? (A)sk for money,(S)teal : ";
-
-
-  // เขียนคำสั่งรับคำตอบจากผู้ใช้ แล้วให้คะแนนหรือทำโทษ จากการตัดสินใจนั้น
-  // เช่น
-  //     cin >> choice1;
-  //     switch(choice1) {
-  //        case "S" : score += 10; life -= 1; cout << "You stole money from people. Now you can shopping.";
-  //        case "A" : score += 50; cout << "You ask for money from people then you get 100 baht. You are very happy. Now you can shopping."
-  //     }
+    // เขียนคำสั่งแสดงผล คำบรรยายสภาพแวดล้อม สถานการณ์ และคำทักทายที่มีชื่อตัวละคร
+    // เช่น cout << "Hello " << playerName << ". You are in Robinson. You are walking happily. Suddenly you realize that forgetting the wallet."
+    // << " But you're lazy to get back to get it." << endl;
 
 
+    // เขียนคำสั่งแสดงผล คำถามให้ผู้ใช้ตัดสินใจ
+    // เช่น cout << "What should you do? (A)sk for money,(S)teal : ";
 
-  // ฝึกสร้างสถานการณ์และสร้างเงื่อนไข อีก 2 สถานการณ์
+
+    // เขียนคำสั่งรับคำตอบจากผู้ใช้ แล้วให้คะแนนหรือทำโทษ จากการตัดสินใจนั้น
+    // เช่น
+    //     cin >> choice1;
+    //     switch(choice1) {
+    //        case "S" : score += 10; life -= 1; cout << "You stole money from people. Now you can shopping.";
+    //        case "A" : score += 50; cout << "You ask for money from people then you get 100 baht. You are very happy. Now you can shopping."
+    //     }
 
 
-  // สรุปผลคะแนนและพลังชีวิต เช่น cout << "Congratulations! You win the game with " << score << "Score. See you next time on next game. GG." << endl;
 
-  return 0;
+    // ฝึกสร้างสถานการณ์และสร้างเงื่อนไข อีก 2 สถานการณ์
+
+
+    // สรุปผลคะแนนและพลังชีวิต เช่น cout << "Congratulations! You win the game with " << score << "Score. See you next time on next game. GG." << endl;
+
+    return 0;
 }
